70f858b06a03eaacb988966011387c5482d5667a
Fanlin Wang <flmaximwang@icloud.com>
2026-05-10 22:18:16 +0800

    Updated RosettaHoles: comments, output pdb

--- biorazer_toolkit/helpers/rosetta/RosettaHoles/RosettaHoles.py
+++ biorazer_toolkit/helpers/rosetta/RosettaHoles/RosettaHoles.py
@@ -1,3 +1,20 @@
+"""
+
+At step 4: Packing filters - Low threshold, designs with Rosetta holes score > 1.75 were filtered out. This is a very lenient threshold, and it is recommended to use a more stringent threshold for future designs.
+At step 7: Packing filters - High threshold, designs with Rosetta holes score > 0.5 were filtered out. This is a more stringent threshold, and it is recommended to use an even more stringent threshold for future designs.
+
+How to interpret the holes score output: 
+A value of 0.0 means on par with native structures observed in the PDB; 
+positive is worse (more voids), negative is better (less voids); 
+this is for the default HolesParams used here, which is dec15; 
+it is not recommended to change this unless you know the inner workings of what this code is doing.
+
+References:
+- /home/wangfanlin1/home/Repositories/PDE002/pipelines/pipeline-3.1.0/2_StructurePrediction/scripts/RosettaHoles
+- Brunette, et al., Exploring the repeat protein universe through computational protein design, Nature, 2015
+"""
+
+
 from pathlib import Path
 import pandas as pd
 from biorazer_toolkit.apps.rosetta import RosettaApp
@@ -28,6 +45,7 @@ for k, v in input_output_map.items():
         "-parser:protocol", f"{rosetta_script_xml}",
         "-nstruct", "1",
         "-out:file:scorefile", f"{v}",
+        "-out:file:pdb", f"{str(Path(v).parent)}",
         mode="subprocess.run"
     )
 
